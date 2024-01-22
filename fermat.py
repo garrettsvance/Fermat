d19734b3d160f7849d16403c2964fe640066e45c
garrettpants <garrettsvance@gmail.com>
2024-01-22 11:17:31 -0700

    coded fermat and mr

--- fermat.py
+++ fermat.py
@@ -1,35 +1,55 @@
+import math
 import random
 
-# This is main function that is connected to the Test button. You don't need to touch it.
+
 def prime_test(N, k):
-	return fermat(N,k), miller_rabin(N,k)
+    return fermat(N, k), miller_rabin(N, k)
 
-# You will need to implement this function and change the return value.
-def mod_exp(x, y, N): 
-	return 1
-	
-# You will need to implement this function and change the return value.   
+
+def mod_exp(x, y, N):
+    if y == 0:
+        return 1
+    z = mod_exp(x, y // 2, N)
+    if y % 2 == 0:
+        return (z**2) % N
+    else:
+        return (x * z**2) % N
+    # mod_exp requires 2 mods per iterations of y bit numbers, and one multiplication.
+    # The runtime of mod_exp is O(y^3)
+
+
 def fprobability(k):
-    return 0.0
+    return 1 - (1/(2**k))
+# Time complexity of fprobability is constant
 
-# You will need to implement this function and change the return value.   
+
 def mprobability(k):
-    return 0.0
+    return 1 - (1/(4**k))
+# Time Complexity of mprobability is constant
 
-# You will need to implement this function and change the return value, which should be
-# either 'prime' or 'composite'.
-#
-# To generate random values for a, you will most likley want to use
-# random.randint(low,hi) which gives a random integer between low and
-# hi, inclusive.
-def fermat(N,k):
-	return 'prime'
 
-# You will need to implement this function and change the return value, which should be
-# either 'prime' or 'composite'.
-#
-# To generate random values for a, you will most likley want to use
-# random.randint(low,hi) which gives a random integer between low and
-#  hi, inclusive.
-def miller_rabin(N,k):
-	return 'composite'
+def fermat(N, k):
+    for i in range(k):
+        a = random.randint(1, N - 1)
+        if mod_exp(a, N - 1, N) != 1:
+            return 'composite'
+    return 'prime'
+# Time Complexity of fermat is 0(N^3) because mod_exp is N^3, and fermat loops through k times
+# k * N^3 = 0(N^3)
+
+
+def miller_rabin(N, k):
+    for i in range(0, k):
+        a = random.randint(1, N - 1)
+        n = N - 1
+        templist = []
+        while (n % 2) == 0:
+            if (mod_exp(a, n, N)) != 1:
+                templist.append(mod_exp(a, n, N))
+            n = n // 2
+        if templist:
+            if templist[0] != N - 1:
+                return 'composite'
+    return 'prime'
+# Time Complexity of miller_rabin is 0(N^4) because we call mod_exp N times
+# N * N^3 = 0(N^4)
